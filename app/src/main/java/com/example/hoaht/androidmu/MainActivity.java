3862ee0134f0dced290c110f2393c2e584112c6e
Hoa Hoang T <hoa.hoang@asiantech.vn>
2017-07-12 09:25:27 +0700

    add android annotations

--- app/src/main/java/com/example/hoaht/androidmu/MainActivity.java
+++ app/src/main/java/com/example/hoaht/androidmu/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.hoaht.androidmu;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * MainActivity
+ *
+ * @author HoaHT
+ */
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
+    @AfterViews
+    void init() {
+
     }
 }
